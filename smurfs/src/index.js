a14bdef3608804d9c94f7496d7c65da592380699
unknown <tommyconner96@gmail.com>
2020-05-01 19:13:47 -0400

    first commit

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { smurfReducer } from './reducers/index.js'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// const store = createStore(smurfReducer, applyMiddleware(thunk))
+
+ReactDOM.render(  <React.StrictMode>
+    {/* <Provider store={store}> */}
+      <App />
+    {/* </Provider> */}
+  </React.StrictMode>, document.getElementById("root"));
